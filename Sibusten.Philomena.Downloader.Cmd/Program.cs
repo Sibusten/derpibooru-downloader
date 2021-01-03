eab0a92f489ed6cc8e359239e01c63905a258901
Sibusten <Sibusten@gmail.com>
2021-01-03 18:35:10 -0500

    Add library and command line project

---
+++ Sibusten.Philomena.Downloader.Cmd/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sibusten.Philomena.Downloader.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Class1.HelloWorld();
+        }
+    }
+}
---
+++ Sibusten.Philomena.Downloader/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sibusten.Philomena.Downloader
+{
+    public class Class1
+    {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello from library");
+        }
+    }
+}
