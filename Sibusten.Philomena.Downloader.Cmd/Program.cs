0c14a9768b0c9212267801aaa86712e33bde2de3
Sibusten <Sibusten@gmail.com>
2021-01-16 17:02:24 -0500

    Move args to submethos

--- Sibusten.Philomena.Downloader.Cmd/Program.cs
+++ Sibusten.Philomena.Downloader.Cmd/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,27 +6,32 @@ namespace Sibusten.Philomena.Downloader.Cmd
 {
     class Program
     {
+        private static string[] GetArgsFromConsole()
+        {
+            Console.WriteLine("Enter program arguments, one per line. Finish by entering an empty line.");
+
+            List<string> args = new List<string>();
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                args.Add(input);
+            }
+
+            return args.ToArray();
+        }
+
         static void Main(string[] args)
         {
 #if DEBUG
             // Prompt for arguments when debugging if none are given
             if (!args.Any())
             {
-                Console.WriteLine("Enter program arguments, one per line. Finish by entering an empty line.");
-
-                List<string> newArgs = new List<string>();
-                while (true)
-                {
-                    string? input = Console.ReadLine();
-                    if (string.IsNullOrEmpty(input))
-                    {
-                        break;
-                    }
-
-                    newArgs.Add(input);
-                }
-
-                args = newArgs.ToArray();
+                args = GetArgsFromConsole();
             }
 #endif
 
