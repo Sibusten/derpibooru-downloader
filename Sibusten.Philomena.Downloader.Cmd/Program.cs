0f9df70198af77ff9c84616dbc9a49d957b59858
Sibusten <Sibusten@gmail.com>
2021-01-16 16:16:21 -0500

    Allow specifying arguments when debugging

--- Sibusten.Philomena.Downloader.Cmd/Program.cs
+++ Sibusten.Philomena.Downloader.Cmd/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Sibusten.Philomena.Downloader.Cmd
 {
@@ -6,6 +8,28 @@ namespace Sibusten.Philomena.Downloader.Cmd
     {
         static void Main(string[] args)
         {
+#if DEBUG
+            // Prompt for arguments when debugging if none are given
+            if (!args.Any())
+            {
+                Console.WriteLine("Enter program arguments, one per line. Finish by entering an empty line.");
+
+                List<string> newArgs = new List<string>();
+                while (true)
+                {
+                    string? input = Console.ReadLine();
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        break;
+                    }
+
+                    newArgs.Add(input);
+                }
+
+                args = newArgs.ToArray();
+            }
+#endif
+
             Console.WriteLine("Hello World!");
 
             Class1.HelloWorld();
