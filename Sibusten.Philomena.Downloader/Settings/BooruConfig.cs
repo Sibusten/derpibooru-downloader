83c136c4ac77ae285b61995110c0fc76656c8328
Sibusten <Sibusten@gmail.com>
2021-01-19 19:27:45 -0500

    Move search configs out of booru config

--- Sibusten.Philomena.Downloader/Settings/BooruConfig.cs
+++ Sibusten.Philomena.Downloader/Settings/BooruConfig.cs
@@ -4,15 +4,14 @@ namespace Sibusten.Philomena.Downloader.Settings
 {
     public class BooruConfig
     {
-        public BooruConfig(string baseUrl)
+        public BooruConfig(string baseUrl, string id)
         {
             BaseUrl = baseUrl;
+            Id = id;
         }
 
         public string BaseUrl { get; set; }
-
+        public string Id { get; set; }
         public string? ApiKey { get; set; }
-
-        public Dictionary<string, SearchConfig> Presets { get; set; } = new Dictionary<string, SearchConfig>();
     }
 }
--- Sibusten.Philomena.Downloader/Settings/Legacy/LegacyConfigFileLoader.cs
+++ Sibusten.Philomena.Downloader/Settings/Legacy/LegacyConfigFileLoader.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using IniParser;
 using IniParser.Model;
 using Newtonsoft.Json;
@@ -8,12 +9,33 @@ namespace Sibusten.Philomena.Downloader.Settings.Legacy
 {
     public class LegacyConfigFileLoader
     {
-        public static BooruConfig LoadLegacyConfigFile(string configFilePath)
+        public static (BooruConfig booruConfig, Dictionary<string, SearchConfig> searchConfigs) LoadLegacyConfigFile(string configFilePath)
         {
             // Read the ini file and load the section where all values are stored
             FileIniDataParser parser = new FileIniDataParser();
             IniData data = parser.ReadFile(configFilePath);
             KeyDataCollection generalSection = data["General"];
+
+            // Ensure API key is null if it is an empty string
+            string? apiKey = generalSection["apiKey"];
+            if (string.IsNullOrWhiteSpace(apiKey))
+            {
+                apiKey = null;
+            }
+
+            // If there is no booru URL, default to Derpibooru
+            string? booruUrl = generalSection["booruUrl"];
+            if (booruUrl is null)
+            {
+                booruUrl = "https://derpibooru.org/";
+            }
+
+            // Name the booru after the domain
+            string booruId = Regex.Match(booruUrl, "https:\\/\\/(.*?)\\/?$").Captures.FirstOrDefault()?.Value ?? "unnamed";
+
+            // Create the booru from legacy settings
+            BooruConfig booruConfig = new BooruConfig(booruUrl, booruId);
+            booruConfig.ApiKey = apiKey;
 
             // TODO: Should the current options be ported?
             // string currentOptions = JsonConvert.DeserializeObject<string>(generalSection["currentOptions"]);
@@ -31,26 +53,7 @@ namespace Sibusten.Philomena.Downloader.Settings.Legacy
                 searchConfigs.Add(kvp.Key, searchConfig);
             }
 
-            // Ensure API key is null if it is an empty string
-            string? apiKey = generalSection["apiKey"];
-            if (string.IsNullOrWhiteSpace(apiKey))
-            {
-                apiKey = null;
-            }
-
-            // If there is no booru URL, default to Derpibooru
-            string? booruUrl = generalSection["booruUrl"];
-            if (booruUrl is null)
-            {
-                booruUrl = "https://derpibooru.org/";
-            }
-
-            // Create the booru from legacy settings
-            BooruConfig booruConfig = new BooruConfig(booruUrl);
-            booruConfig.ApiKey = apiKey;
-            booruConfig.Presets = searchConfigs;
-
-            return booruConfig;
+            return (booruConfig, searchConfigs);
         }
     }
 }
