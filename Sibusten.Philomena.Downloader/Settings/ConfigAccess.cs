c4146a758a4169ffef11271a62091ab802846221
Sibusten <Sibusten@gmail.com>
2021-01-23 12:47:15 -0500

    Add ConfigAccess class for accessing the database

---
+++ Sibusten.Philomena.Downloader/Settings/ConfigAccess.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LiteDB;
+
+namespace Sibusten.Philomena.Downloader.Settings
+{
+    public class ConfigAccess
+    {
+        private const string configFilename = "PhilomenaDownloader.db";
+        private const string booruCollectionName = "boorus";
+        private const string presetCollectionName = "presets";
+
+        ILiteDatabase _db;
+        private ILiteCollection<BooruConfig> _booruCollection;
+        private ILiteCollection<SearchPreset> _presetCollection;
+
+        public ConfigAccess()
+        {
+            string configPath = Path.Join(Directory.GetCurrentDirectory(), configFilename);
+            _db = new LiteDatabase(configPath);
+
+            _booruCollection = _db.GetCollection<BooruConfig>(booruCollectionName);
+            _presetCollection = _db.GetCollection<SearchPreset>(presetCollectionName);
+        }
+
+        public List<BooruConfig> GetBoorus()
+        {
+            return _booruCollection.FindAll().ToList();
+        }
+
+        public List<SearchPreset> GetPresets()
+        {
+            return _presetCollection.FindAll().ToList();
+        }
+    }
+}
