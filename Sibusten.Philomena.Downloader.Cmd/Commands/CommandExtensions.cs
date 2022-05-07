755907e2181f00f1fa93280880297d2e0805cb5e
Sibusten <Sibusten@gmail.com>
2022-05-07 00:30:24 -0400

    Simplify arguments for download command

--- Sibusten.Philomena.Downloader.Cmd/Commands/CommandExtensions.cs
+++ Sibusten.Philomena.Downloader.Cmd/Commands/CommandExtensions.cs
@@ -16,29 +16,5 @@ namespace Sibusten.Philomena.Downloader.Cmd.Commands
             command.Handler = handler;
             return command;
         }
-
-        public static Command WithSearchQueryArgs(this Command command)
-        {
-            List<Symbol> SearchQueryArgs = new List<Symbol>
-            {
-                new Option<string>(new[] { "--query", "-q" }, "The search query"),
-                new Option<int>(new[] { "--limit", "-l" }, "The maximum number of images to download. Defaults to all images"),
-                new Option<int>(new[] { "--filter", "-f" }, "The ID of the filter to use"),
-                new Option<string>(new[] { "--image-path", "-I" }, "Where to save images and how to name them"),
-                new Option<string>(new[] { "--json-path", "-J" }, "Where to save json files and how to name them"),
-                new Option<bool?>(new[] { "--skip-images", "-i" }, "Skip saving images"),
-                new Option<bool>(new[] { "--save-json", "-j" }, "Save json metadata files"),
-                new Option<bool>(new[] { "--update-json", "-u" }, "Overwrite json metadata files with new data"),
-                new Option<List<string>>(new[] { "--boorus", "-b" }, "What booru to download from. Multiple boorus can be given"),
-                new Option<SvgMode>(new[] { "--svg-mode", "-g" }, "How to download SVG images"),
-            };
-
-            foreach (Symbol arg in SearchQueryArgs)
-            {
-                command.Add(arg);
-            }
-
-            return command;
-        }
     }
 }
--- Sibusten.Philomena.Downloader.Cmd/Commands/DownloadCommand.cs
+++ Sibusten.Philomena.Downloader.Cmd/Commands/DownloadCommand.cs
@@ -17,7 +17,17 @@ namespace Sibusten.Philomena.Downloader.Cmd.Commands
             return new Command("download", "Search for and download images.")
             {
                 new Option<string>(new[] { "--api-key", "-a" }, "The API key to use"),
-            }.WithSearchQueryArgs().WithHandler(new Func<DownloadArgs, Task>(DownloadCommandFunc));
+                new Option<string>(new[] { "--query", "-q" }, "The search query"),
+                new Option<int>(new[] { "--limit", "-l" }, "The maximum number of images to download. Defaults to all images"),
+                new Option<int>(new[] { "--filter", "-f" }, "The ID of the filter to use"),
+                new Option<string>(new[] { "--image-path", "-I" }, "Where to save images and how to name them"),
+                new Option<string>(new[] { "--json-path", "-J" }, "Where to save json files and how to name them"),
+                new Option<bool?>(new[] { "--skip-images", "-i" }, "Skip saving images"),
+                new Option<bool>(new[] { "--save-json", "-j" }, "Save json metadata files"),
+                new Option<bool>(new[] { "--update-json", "-u" }, "Overwrite json metadata files with new data"),
+                new Option<List<string>>(new[] { "--booru", "-b" }, "What booru to download from"),
+                new Option<SvgMode>(new[] { "--svg-mode", "-g" }, "How to download SVG images"),
+            }.WithHandler(new Func<DownloadArgs, Task>(DownloadCommandFunc));
         }
 
         private async Task DownloadCommandFunc(DownloadArgs args)
