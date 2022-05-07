7073b77c3b047eda76d969005079d117879b7b2f
Sibusten <Sibusten@gmail.com>
2022-05-07 00:25:52 -0400

    Remove unused utility

--- Sibusten.Philomena.Downloader/Utility/UrlUtilities.cs
+++ Sibusten.Philomena.Downloader/Utility/UrlUtilities.cs
@@ -1,21 +1,9 @@
 using System;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace Sibusten.Philomena.Downloader.Utility
 {
     public static class UrlUtilities
     {
-        private static string _domainNameRegex = @"https?:\/\/(.+?)(?:\/|$)";
-
-        public static string? GetDomain(string url)
-        {
-            Match match = Regex.Match(url, _domainNameRegex);
-
-            // Return the first capture group (domain name), or null if it doesn't exist
-            return match.Groups.Cast<Group>().ElementAtOrDefault(1)?.Value;
-        }
-
         public static Uri GetWellFormedWebUri(string rawUri)
         {
             // If the URL is valid, use it
